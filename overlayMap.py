83db8f58fc3dd63b4dbb591658af7d98729c222f
Ioannis Georgilas <ig358@bath.ac.uk>
2024-10-09 18:13:57 +0100

    Fixed the overlayMap function
    The original version was using the corners of the Aruco marker.
    This was changed to find the point between the two opposite corners and use that as the center of the circle.
    
    Added the .gitignore file to ignore the __pycache__ folder and the .pyc files.

--- overlayMap.py
+++ overlayMap.py
@@ -4,12 +4,23 @@ def overlayMap(frame, map_data):
     babySpice = map_data.get('babySpice', [])
     sandworm = map_data.get('sandworm', [])
 
+    # The two objects are the corners of the ArUco markers on the image plane
+    # We need to find the point in the middle of the first and the third corner to overlay the object
+    # Check if babySpice and sandworm are not empty and are lists
+    if babySpice and isinstance(babySpice, list):
+        for i in babySpice:
+            babySpicecircle = [(int((i[0][0][0] + i[0][2][0]) / 2), int((i[0][0][1] + i[0][2][1]) / 2))]
+                               
+    if sandworm and isinstance(sandworm, list):
+        for i in sandworm:
+            sandwormcircle = [(int((i[0][0][0] + i[0][2][0]) / 2), int((i[0][0][1] + i[0][2][1]) / 2))]
+
     # Overlay babySpice and sandworm on the frame if they are not empty
-    if babySpice and isinstance(babySpice, list) and all(isinstance(i, list) and len(i) == 2 for i in babySpice):
-        for (x, y) in babySpice:
+    if babySpicecircle and isinstance(babySpicecircle, list) and all(len(i) == 2 for i in babySpicecircle):
+        for (x, y) in babySpicecircle:
             cv2.circle(frame, (x, y), 100, (255, 0, 0), -1)  
-    if sandworm and isinstance(sandworm, list) and all(isinstance(i, list) and len(i) == 2 for i in sandworm):
-        for (x, y) in sandworm:
+    if sandwormcircle and isinstance(sandwormcircle, list) and all(len(i) == 2 for i in sandwormcircle):
+        for (x, y) in sandwormcircle:
             cv2.circle(frame, (x, y), 100, (0, 0, 255), -1)  
 
     return frame
